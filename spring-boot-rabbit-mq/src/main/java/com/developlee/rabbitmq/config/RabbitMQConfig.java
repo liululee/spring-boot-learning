ef8e88812339ac1f5cde0399b16a4e88ff814e94
developlee <developlee@163.com>
2019-05-22 23:11:53 +0800

    spring-boot集成rabbit-mq消息队列

--- spring-boot-rabbit-mq/src/main/java/com/developlee/rabbitmq/config/RabbitMQConfig.java
+++ spring-boot-rabbit-mq/src/main/java/com/developlee/rabbitmq/config/RabbitMQConfig.java
@@ -12,13 +12,13 @@ public class RabbitMQConfig {
     public static final String MANUAL_MAIL_QUEUE = "dev.mail.register.manual.queue";
 
     @Bean
-    public Queue defaultMailQueue (){
+    public Queue defaultMailQueue() {
         // Queue queue = new  Queue(Queue名称，消息是否需要持久化处理)
         return new Queue(DEFAULT_MAIL_QUEUE, true);
     }
 
     @Bean
-    public Queue manualMailQueue(){
+    public Queue manualMailQueue() {
         return new Queue(MANUAL_MAIL_QUEUE, true);
     }
 }
