9110bc9b24b67b64d6aed51197dc1360932ce120
developlee <developlee@163.com>
2019-05-22 23:06:36 +0800

    spring-boot集成Redis缓存

--- spring-boot-redis/src/main/java/com/developlee/redis/service/UserService.java
+++ spring-boot-redis/src/main/java/com/developlee/redis/service/UserService.java
@@ -3,5 +3,5 @@ package com.developlee.redis.service;
 import com.developlee.redis.entity.UserEntity;
 
 public interface UserService {
-    UserEntity findUserById (Long id);
+    UserEntity findUserById(Long id);
 }
