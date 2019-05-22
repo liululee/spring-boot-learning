12594727299d5d18e2ea339fdbf261b78e177119
developlee <developlee@163.com>
2019-05-22 23:31:08 +0800

    spring-boot整合JPA多数据源

---
+++ spring-boot-multipart-jpa/src/main/java/com/developlee/multipartjpadatasource/MultipartJpaDatasourceApplication.java
@@ -0,0 +1,12 @@
+package com.developlee.multipartjpadatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipartJpaDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipartJpaDatasourceApplication.class, args);
+    }
+}
