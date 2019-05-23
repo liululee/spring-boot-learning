5e7d07f63f07bbc5fe6135a1d4f44a9cbff39707
developlee <developlee@163.com>
2019-05-23 08:24:14 +0800

    spring-security自定义认证流程

--- spring-security-authentication/src/main/java/com/developlee/springsecuritypart3/config/SecurityConfig.java
+++ spring-security-authentication/src/main/java/com/developlee/springsecuritypart3/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
