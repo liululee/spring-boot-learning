2a4fb781752d37b35f3f0349ca81cba218928d89
liululee <luli.liu@guuidea.com>
2019-06-14 15:15:31 +0800

    spring-boot:消息转换

---
+++ spring-boot-message-converter/src/main/java/com/developlee/springbootmessageconverter/SpringBootMessageConverterApplication.java
@@ -0,0 +1,13 @@
+package com.developlee.springbootmessageconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMessageConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMessageConverterApplication.class, args);
+    }
+
+}
