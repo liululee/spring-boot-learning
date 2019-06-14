d5250b00653d4e510ad2a1de4edf7e598d5b9d6f
liululee <luli.liu@guuidea.com>
2019-06-14 17:38:21 +0800

    spring-social: github登录

---
+++ spring-social-github/src/main/java/com/developlee/springsocialgithub/SpringSocialGithubApplication.java
@@ -0,0 +1,13 @@
+package com.developlee.springsocialgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocialGithubApplication.class, args);
+    }
+
+}
