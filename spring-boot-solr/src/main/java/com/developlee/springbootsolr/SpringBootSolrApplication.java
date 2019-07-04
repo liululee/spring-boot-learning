5c7b9537682e254acc194ebf144473752aadf034
liululee <luli.liu@guuidea.com>
2019-07-04 17:58:45 +0800

    spring-boot: solr搜索

---
+++ spring-boot-solr/src/main/java/com/developlee/springbootsolr/SpringBootSolrApplication.java
@@ -0,0 +1,13 @@
+package com.developlee.springbootsolr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSolrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSolrApplication.class, args);
+    }
+
+}
