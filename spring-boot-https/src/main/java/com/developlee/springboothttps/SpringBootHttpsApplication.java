86897a2c47468043267331c8b2a82ed087d72748
liululee <luli.liu@guuidea.com>
2019-11-29 14:10:30 +0800

    springboot: clear code

---
+++ spring-boot-https/src/main/java/com/developlee/springboothttps/SpringBootHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.developlee.springboothttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHttpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHttpsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-https/src/main/java/com/developlee/springboothttps/config/TomcatConfig.java
@@ -0,0 +1,36 @@
+package com.developlee.springboothttps.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liululee
+ * @desc
+ * @since 6/26/2019
+ **/
+@Configuration
+public class TomcatConfig {
+
+    @Value("${server.http.port}")
+
+    private int port;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }
+
+    // 配置http
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        return connector;
+    }
+
+}
