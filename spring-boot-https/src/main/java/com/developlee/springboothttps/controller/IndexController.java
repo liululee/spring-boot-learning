b750b4575e3a71bb9bb116a14ea482bb81f11579
liululee <liululee@163.com>
2020-03-06 11:08:03 +0800

    spring boot: 支持https

---
+++ spring-boot-https/src/main/java/com/developlee/springboothttps/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.developlee.springboothttps.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "Hello, index!";
+    }
+}
